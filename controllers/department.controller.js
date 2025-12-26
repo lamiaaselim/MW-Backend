0108c9429bd1f007f2b6859562f502d5ee4d2362
Lamiaa Selim <l.selim@zad-solutions.com>
2025-12-26 15:20:09 +0200

    refactor: department controller logic using async, await and try, catch block

--- controllers/department.controller.js
+++ controllers/department.controller.js
@@ -3,59 +3,57 @@
  * Build Business Logic => CRUD Operation on Entity
  */
 
-const DepartmentSchema = require('./../models/department.model')
+const DepartmentSchema = require("./../models/department.model");
 
-exports.getAllDepartments = (req, res, next) => {
-    // extract query string 
-    // console.log(req.query);
-    // console.log(req.query.id);
-    DepartmentSchema.find({})
-        .then((departments) => {
-            res.status(200).json({ data: departments })
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
-// break : 4:30 
-exports.createDepartment = (req, res, next) => {
-    // console.log(req.body);
-    let newDepart = new DepartmentSchema({
-        _id: req.body._id,
-        name: req.body.name
-    })
-    newDepart.save()
-        .then((newObj) => {
-            res.status(200).json({ data: "Department Added ", newObj })
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+exports.getAllDepartments = async (req, res, next) => {
+    try {
+        const departs = await DepartmentSchema.find({});
+        res.status(200).json({ data: departs });
+    } catch (err) {
+        next(err);
+    }
+};
+exports.createDepartment = async (req, res, next) => {
+    try {
+        const newDepart = new DepartmentSchema({
+            _id: req.body._id,
+            name: req.body.name,
+        });
+        const newObj = await newDepart.save();
+        res.status(200).json({ data: "Department Added ", newObj });
+    } catch (err) {
+        next(err);
+    }
+};
 
-exports.updateDepartment = (req, res, next) => {
-    DepartmentSchema.updateOne({ _id: req.body._id }, { $set: { name: req.body.name } })
-        .then((updatedDepart) => {
-            res.status(200).json({ data: "Department Updated", updatedDepart })
-        })
-        .catch((err) => { next(err) })
-}
+exports.updateDepartment = async (req, res, next) => {
+    try {
+        const updatedDepart = await DepartmentSchema.updateOne(
+            { _id: req.body._id },
+            { $set: { name: req.body.name } }
+        );
+        res.status(200).json({ data: "Department Updated", updatedDepart });
+    } catch (err) {
+        next(err);
+    }
+};
 
-exports.getOneDepartment = (req, res, next) => {
-    // extract parameter
-    // console.log(req.params);
-    // console.log(req.params.id);
-    DepartmentSchema.findOne({ _id: req.params.id })
-        .then((depart) => {
-            // check if  depart is exist
-            if (depart == null) {
-                throw new Error('Department not Exist')
-            }
-            res.status(200).json({ data: depart })
-        })
-        .catch((err) => { next(err) })
-}
+exports.getOneDepartment = async (req, res, next) => {
+    try {
+        const depart = await DepartmentSchema.findOne({ _id: req.params.id });
+        if (!depart) {
+            const error = new Error("Department Not Exist");
+            error.status = 404;
+            throw error;
+        }
+        res.status(200).json({ data: depart });
+    } catch (err) { next(err); }
 
-exports.deleteDepartment = (req, res, next) => {
-    res.status(200).json({ data: "Department Deleted" })
-}
+};
+
+exports.deleteDepartment = async (req, res, next) => {
+    try {
+        await DepartmentSchema.deleteOne({ _id: req.params.id });
+        res.status(200).json({ data: "Department Deleted" });
+    } catch (err) { next(err); }
+};
